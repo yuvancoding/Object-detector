80587a58aee7a4c27b1c93761528b21fcbf7f73b
yuvancoding <112766393+yuvancoding@users.noreply.github.com>
2023-06-10 19:38:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+objectDetector="";
+
+img = "";
+objects = [];
+status1 = "";
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status1").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(status1 != "")
+    {
+        for(var i= 0; i < objects.length; i++) {
+            document.getElementById("status1").innerHTML = "Status : Object Detected";
+
+            fill(255, 0, 0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label +" " + percent + "%", objects[i].x +15, objects[i].y + 15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
